5d72dedd77315b7d056ff0c782a7848640728ad5
erpesh <89254828+erpesh@users.noreply.github.com>
2023-06-05 15:16:22 +0100

    fix: accessibility

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -42,10 +42,10 @@ const ProjectCard = ({project} : { project: Project }) => {
             {inProgress && <p className={"card-progress"}><RiToolsLine/> Dev Mode</p>}
           </div>
           <div className={"flex justify-between items-start gap-3 pt-1"}>
-            {!!link && <a target="_blank" href={link} rel="noopener noreferrer" className={"card-link"}>
+            {!!link && <a target="_blank" href={link} rel="noopener noreferrer" className={"card-link"} aria-label={"Website link"}>
               <FaExternalLinkAlt size={24}/>
             </a>}
-            <a target="_blank" href={githubLink} rel="noopener noreferrer" className={"card-link"}>
+            <a target="_blank" href={githubLink} rel="noopener noreferrer" className={"card-link"} aria-label={"GitHub"}>
               <FaGithub size={28}/>
             </a>
           </div>
