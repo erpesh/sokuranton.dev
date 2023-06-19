f503db994e5df2817c11ba1372f480d032426809
erpesh <89254828+erpesh@users.noreply.github.com>
2023-06-19 13:55:28 +0100

    feat: link to projects page on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import {FaEnvelope, FaGithub, FaLinkedin, FaTelegramPlane} from 'react-icons/fa';
 import ParticlesBackground from "../components/particles-background";
 import {Metadata} from "next";
+import {FiArrowRight} from "react-icons/all";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Home | Anton Sokur",
@@ -18,8 +20,9 @@ const Page = () => {
       <ParticlesBackground fullScreen/>
       <div className="content">
         <h1>Anton Sokur</h1>
-        <span
-          className={"sec-head"}><span>Software Engineer,</span><span>Web Developer,</span><span>University Student</span></span>
+        <span className={"sec-head"}>
+          <span>Software Engineer,</span><span>Web Developer,</span><span>University Student</span>
+        </span>
         <div className={"links-container gap-4 mt-4"}>
           <a target="_blank" href="https://t.me/erpesh" rel="noopener noreferrer" aria-label={"Telegram"}>
             <FaTelegramPlane size={32}/>
@@ -34,6 +37,9 @@ const Page = () => {
              href={"mailto:anton.sokur1@gmail.com?subject=Hello%20there&body=I%20found%20your%20website%20and..."} aria-label={"Email"}>
             <FaEnvelope size={32}/>
           </a>
+        </div>
+        <div className={"to-projects"}>
+          <Link href={"/projects"}>My Projects <FiArrowRight size={26}/></Link>
         </div>
       </div>
     </div>
